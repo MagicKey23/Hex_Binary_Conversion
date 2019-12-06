c6b39889ab1dda254cc64325b84b7f214ba56c75
MagicKey23 <kaneyz112@gmail.com>
2019-12-05 23:27:11 -0500

    Add a hex to decimal

--- Hex.rb
+++ Hex.rb
@@ -4,23 +4,16 @@ class Hexdecimal
 # Get the remainder for the Hexdecimal Digit.
 # Repeat the steps until the quotient is equal to 0.
 
+ HEX_VALUE = { 
+        "A" => 10,
+        "B" => 11,
+        "C" => 12,
+        "D" => 13,
+        "E" => 14,
+        "F" => 15
+    }
 
- def convert_to_hex(decimal)
-
-        if self.is_hex?(decimal)
-            if decimal < 10
-                return HEX_TABLE[decimal]
-            elsif decimal >= 10 && decimal < 16
-                return HEX_TABLE[decimal]
-            end
-        else
-            return self.hex(self.conversion(decimal), self.isNegative?(decimal))
-        end
-    end
-
-   protected
-
-   HEX_TABLE =  {
+     HEX_TABLE =  {
        1 => 1,
        2 => 2,
        3 => 3,
@@ -38,6 +31,52 @@ class Hexdecimal
        15 => "F"
    }
 
+ def convert_to_hex(decimal)
+
+     
+        if self.is_hex?(decimal)
+            if decimal < 10
+                return HEX_TABLE[decimal]
+            elsif decimal >= 10 && decimal < 16
+                return HEX_TABLE[decimal]
+            end
+        else
+            return self.hex(self.conversion(decimal), self.isNegative?(decimal))
+        end
+    end
+
+       def  convert_to_decimal(number)
+        # EAT MEMORY BUT W/e :(
+      
+        # Check
+        if number.length < 2
+            return HEX_VALUE[number] if HEX_VALUE.has_key?(number)
+            return number
+        end
+
+        # Assign Variables
+        hex = number.split("").reverse
+        pos = hex.length
+        sum = 0 
+        # Go through each number
+        while pos > 0
+            # Find the Original Value 
+            HEX_VALUE.has_key?(hex[pos-1]) ? value = HEX_VALUE[hex[pos-1]]: value = hex[pos-1]
+            
+            result = self.calculate_hex_value(pos-1, value)
+             # Add up 
+            sum += result
+            pos -= 1
+        end 
+        # return the sum
+        return sum
+         
+    end
+
+   protected
+
+  
+    
 
    def isNegative?(n)
         if n < 0 
@@ -83,6 +122,8 @@ class Hexdecimal
 
     end
 
-    
+    def calculate_hex_value(pos, number)
+        return 16**pos.to_i * number.to_i
+    end
 end
 
--- Machine.rb
+++ Machine.rb
@@ -3,43 +3,63 @@ require_relative "Binary"
 
 class Machine
 
+
     def run_binary(num)
-          
             puts "Select the type of conversion\n 1. Binary to Decimal \n 2. Decimal to Binary"
             selector2 = gets.chomp.to_i
+            system("clear")
             if selector2 > 2 || selector2 < 1
                 puts "Invalid choice, please select again"
                 run_binary(num)
             end
-        
+
+
             init = Binary.new
 
             if selector2 == 1
 
-            result = init.convert_binary_to_decimal(num)
+            result = init.convert_binary_to_decimal(num.to_i)
 
             elsif selector2 == 2
 
 
-            result = init.convert_to_binary(num)
+            result = init.convert_to_binary(num.to_i)
 
             end
-            puts "The result is: #{result}"
 
-            
+            puts "The result is: #{result}"
+    
+
     end
 
     def run_hex(num) 
+            puts "Select the type of conversion\n 1. Hex to Decimal \n 2. Decimal to Hex"
+            selector2 = gets.chomp.to_i
+            system("clear")
+            if selector2 > 2 || selector2 < 1
+                puts "Invalid choice, please select again"
+                run_hex(num)
+            end
+
+
             init = Hexdecimal.new
-            result = init.convert_to_hex(num)
+            if selector2 == 1 
+                result = init.convert_to_decimal(num)  
+            elsif selector2 == 2
+                result = init.convert_to_hex(num.to_s)
+            end
+           
+        
             puts "The result is: #{result}"
+
     end
+
 
     def start 
 
         puts "Please enter the number to convert"
 
-        input = gets.chomp.to_i
+        input = gets.chomp
 
         puts "Please select which conversion do you want\n1. Binary \n2. Hexdecimal"
 
@@ -52,11 +72,13 @@ class Machine
 
         if selector == 1 
             run_binary(input)
-        elsif selector2 == 2
+        elsif selector == 2
             run_hex(input)
         end
 
     end 
+
+end
 
 if $PROGRAM_NAME == __FILE__
      machine = Machine.new  
